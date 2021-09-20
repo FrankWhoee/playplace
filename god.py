1f0526919c73623520bb1952a288aa6ed2464400
Frank Hui <huifrank@live.com>
2021-09-20 12:48:29 -0700

    Become religious

---
+++ god.py
@@ -0,0 +1,15 @@
+import random
+def godsort(array):
+	for i in range(len(array)):
+		s = findMin(array,i)
+		array[s],array[i] = array[i],array[s]
+	return array
+		
+def findMin(array,i):
+	return random.randint(i,len(array)-1)
+	
+# Assumes array is already sorted
+def bestsort(array):
+	return array
+
+print(godsort([5,6,8,13,2,1,2,5,6,2]))
